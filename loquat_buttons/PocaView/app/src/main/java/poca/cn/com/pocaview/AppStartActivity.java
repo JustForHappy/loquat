adde620004dadac8d7ff6e2aa9a07e3ceeb8188e
jiangchangzheng <jiancz@jiedaibao.com>
2016-06-07 14:22:42 +0800

    添加build下的非class 文件的文件

---
+++ loquat_buttons/PocaView/app/src/main/java/poca/cn/com/pocaview/AppStartActivity.java
@@ -0,0 +1,13 @@
+package poca.cn.com.pocaview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_start);
+    }
+}
