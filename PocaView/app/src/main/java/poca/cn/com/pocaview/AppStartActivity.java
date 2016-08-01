cf9784d66d0286edf4ecd39dbd5ec25681913929
蒋昌政 <jiancz@jiedaibao.com>
2016-08-01 23:12:22 +0800

    调整ignore文件

--- PocaView/app/src/main/java/poca/cn/com/pocaview/AppStartActivity.java
+++ PocaView/app/src/main/java/poca/cn/com/pocaview/AppStartActivity.java
@@ -2,6 +2,7 @@ package poca.cn.com.pocaview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
 
 public class AppStartActivity extends Activity {
 
@@ -9,5 +10,7 @@ public class AppStartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_start);
+
+        SystemClock.sleep(300 * 1000);
     }
 }
